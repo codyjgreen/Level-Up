1e42e8522c9004649f74b9676d0e9b2774974d69
Cody Green <codyjeromegreen@gmail.com>
2022-03-10 11:02:56 -0800

    got file structure and basics framed out

--- index.js
+++ index.js
@@ -1,14 +1,58 @@
 // Require the necessary discord.js classes
-const { Client, Intents } = require('discord.js');
+const fs = require('node:fs');
+const { Client, Collection, Intents } = require('discord.js');
 const { token } = require('./config.json');
 
 // Create a new client instance
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
+// Create a new collection for the commands
+client.commands = new Collection();
+
+// Create a new collection for the events
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+	for (const file of commandFiles) {
+		const command = require(`./commands/${file}`);
+		// Set a new item in the collection
+		// with the key as the command name and the value as the exported module
+		client.commands.set(command.data.name, command);
+	}
+
+
+	const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
+
+	for (const file of eventFiles) {
+		const event = require(`./events/${file}`);
+		if (event.once) {
+			client.once(event.name, (...args) => event.execute(...args));
+		} else {
+			client.on(event.name, (...args) => event.execute(...args));
+		}
+	}
+
 // When the client is ready, run this code (only once)
-client.once('ready', () => {
-	console.log('Ready!');
+// client.once('ready', () => {
+// 	console.log('Ready!');
+// });
+
+
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isCommand()) return;
+
+	const command = client.commands.get(interaction.commandName);
+
+	if (!command) return;
+
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+	}
 });
+
+
 
 
 // Login to Discord with your client's token
